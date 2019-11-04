ecb78cb726e232e4bf6ac73a6a283c376836087c
legacy-gopher-9954 <fabianjohnson07@gmail.com>
2019-11-04 23:24:07 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,14 +4,14 @@ def second_challenge
       #your key/value pairs here!
       :patriarch => {:name "Lord Montague", :age ="53" },
       :matriarch => {:name "Lady Montague"},
-      :hero => {},
+      :hero => {:name "Romeo", :age "54", :status "alive"},
       :hero_friends => [:name "Mercutio", :age "18",
        :attitude "hot-headed"]
       }, 
     :capulet => {
       #your key/value pairs here!
-      :patriarch => {}
-      :matriarch => {}
+      :patriarch => {:name "Lord Capulet", :age "50", }
+      :matriarch => {:name "Lady Capulet",}
       :heroine => {}
       :heroine_freiends =>[]
       }}
