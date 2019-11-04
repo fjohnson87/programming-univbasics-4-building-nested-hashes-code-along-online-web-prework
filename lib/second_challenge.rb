591d9c7ee88977d43bfa307dc9f855e0eda4da94
legacy-gopher-9954 <fabianjohnson07@gmail.com>
2019-11-04 23:28:06 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -15,9 +15,9 @@ def second_challenge
       :patriarch => {:name "Lord Capulet", :age "50", }
       :matriarch => {:name "Lady Capulet", :age "51"}
       :heroine => {:name "Juliet" , :age "15", :status "alive"}
-      :heroine_freiends =>[[:name "Mercutio", :age "18",
-       :attitude "hot-headed"],[:name "Mercutio", :age "18",
-       :attitude "hot-headed"]
+      :heroine_freiends =>[[:name "Steven", :age "30",
+       :attitude "confused"],[:name "Nurse", :age "44",
+       :attitude "worried"]
       }}
 
   
