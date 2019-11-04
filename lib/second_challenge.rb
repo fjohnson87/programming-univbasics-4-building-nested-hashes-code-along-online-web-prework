aa64f26751fbbd7d2ca47d7750400138dcd0760e
legacy-gopher-9954 <fabianjohnson07@gmail.com>
2019-11-04 23:22:25 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,10 +2,11 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
-      :patriarch => {:name "Lord Montague", :age = },
-      :matriarch => {},
+      :patriarch => {:name "Lord Montague", :age ="53" },
+      :matriarch => {:name "Lady Montague"},
       :hero => {},
-      :hero_friends => []
+      :hero_friends => [:name "Mercutio", :age "18",
+       :attitude "hot-headed"]
       }, 
     :capulet => {
       #your key/value pairs here!
